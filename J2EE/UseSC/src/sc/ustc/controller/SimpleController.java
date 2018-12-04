1e65fac22d2872f8665261e8cce3995faf0e5426
xq773939719 <773939719@qq.com>
2018-12-04 10:34:15 +0800

    修改

---
+++ J2EE/UseSC/src/sc/ustc/controller/SimpleController.java
@@ -0,0 +1,21 @@
+package sc.ustc.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SimpleController")
+public class SimpleController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+}
---
+++ J2EE/UseSC/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xq
+  Date: 2018-11-30
+  Time: 00:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>你好</title>
+</head>
+<body>
+欢迎使用SimpleController
+</body>
+</html>
