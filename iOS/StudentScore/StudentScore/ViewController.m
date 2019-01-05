e5e4768cdc29e6a1c426eaf5dd9ca68dc0f2d921
许强 <xq@XQdeMacBook-Pro.local>
2019-01-05 20:44:16 +0800

    modify

--- iOS/StudentScore/StudentScore/ViewController.m
+++ iOS/StudentScore/StudentScore/ViewController.m
@@ -22,23 +22,57 @@
 - (IBAction)enter_428:(id)sender {
     TableViewController * tc = [[TableViewController alloc]init];
     Student * student = [[Student alloc] init];
-    student.name = self.name_428.text;
-    student.number = self.number_428.text;
-    student.age = [self.age_428.text integerValue];
-    student.score = [self.score_428.text floatValue];
-    student.memo = self.memo_428.text;
-    student.teacher = @"白天";
     
-    if(self.indexPath == nil){
-        [self.students addObject:student];
-        [tc writeToFile:self.students filePath:self.path];
+    if([self.name_428.text isEqualToString:@"" ]|| [self.number_428.text isEqualToString:@""] || [self.age_428.text isEqualToString:@""] || [self.score_428.text isEqualToString:@""] || [self.score_428.text integerValue] < 0 || [self.score_428.text integerValue] > 100 || [self.age_428.text integerValue] < 0 || [self.age_428.text integerValue] > 100){
+        
+        UIAlertController * uiAlertController = [UIAlertController alertControllerWithTitle:@"出现错误辣(>_<)" message:@"输入有误，请输入合法格式" preferredStyle:UIAlertControllerStyleAlert];
+        //
+        UIAlertAction * uiYesAction = [UIAlertAction actionWithTitle:@"好的👌" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            NSLog(@"好的👌");
+        }];
+        /*
+         UIAlertAction * uiNoAction = [UIAlertAction actionWithTitle:@"不行🚫" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+         NSLog(@"不行🚫");
+         
+         }];
+         */
+        //
+        [uiAlertController addAction:uiYesAction];
+        //[uiAlertController addAction:uiNoAction];
+        //
+        [self presentViewController:uiAlertController animated:YES completion:nil];
+        
+        
+        
+        
+        
+        
+        self.name_428.text = nil;
+        self.number_428.text = nil;
+        self.age_428.text = nil;
+        self.score_428.text = nil;
+        self.memo_428.text = nil;
     }
     else{
-        self.students[self.indexPath.row] = student;
-        [tc writeToFile:self.students filePath:self.path];
+        student.name = self.name_428.text;
+        student.number = self.number_428.text;
+        student.age = [self.age_428.text integerValue];
+        student.score = [self.score_428.text floatValue];
+        student.memo = self.memo_428.text;
+        student.teacher = @"白天";
+        
+        if(self.indexPath == nil){
+            [self.students addObject:student];
+            [tc writeToFile:self.students filePath:self.path];
+        }
+        else{
+            self.students[self.indexPath.row] = student;
+            [tc writeToFile:self.students filePath:self.path];
+        }
+        
+        [self.navigationController popToRootViewControllerAnimated:YES];
     }
     
-    [self.navigationController popToRootViewControllerAnimated:YES];
     
     
     
@@ -56,6 +90,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.name_428.autocorrectionType = UITextAutocorrectionTypeYes;
 
 
 }
